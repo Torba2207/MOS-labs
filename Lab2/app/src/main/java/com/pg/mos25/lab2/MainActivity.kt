bae8a4b18d33a15a5da9dc9f6dc06853c8034601
Torba2207 <alexnychyporchuk@gmail.com>
2025-03-13 17:45:54 +0100

    add: Main Activity now has 2 switches(one for color of CustomView and another for the shape) and Updated ProceedButton to proceed also values about color and shape

--- Lab2/app/src/main/java/com/pg/mos25/lab2/MainActivity.kt
+++ Lab2/app/src/main/java/com/pg/mos25/lab2/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.pg.mos25.lab2.components.ProceedButton
 import com.pg.mos25.lab2.ui.theme.Lab2Theme
 
@@ -21,9 +29,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Lab2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                val isRed = remember { mutableStateOf(false) }
+                val isRectangle = remember { mutableStateOf(false) }
+                Column (modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text("Change Shape and Color", fontSize = 20.sp)
+
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text("Red Color")
+                        Switch(
+                            checked = isRed.value,
+                            onCheckedChange = { checked -> isRed.value=checked }
+                        )
+                    }
+
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text("Rectangle Shape")
+                        Switch(
+                            checked = isRectangle.value,
+                            onCheckedChange = { checked -> isRectangle.value=checked }
+                        )
+                    }
+
+
                     ProceedButton("To the Second Activity", LocalContext.current,
-                        SecondActivity::class.java, modifier = Modifier.padding(innerPadding))
+                        SecondActivity::class.java,
+                        isRed=isRed.value,
+                        isRectangle=isRectangle.value)
                 }
             }
         }
